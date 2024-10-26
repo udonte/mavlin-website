b266fe54ece70261afa9c31b8ac812c75c2a7192
George Udonte <georgeudonte@gmail.com>
2024-10-26 16:55:49 +0100

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,47 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import Mavlinlogo from "../assets/images/mavlinBlue.png";
 import { motion } from "framer-motion";
 
-const Header = () => (
-  <motion.nav
-    initial={{ y: -100 }}
-    animate={{ y: 0 }}
-    transition={{ duration: 0.5 }}
-    className="bg-white text-mavlin-blue px-24 py-4 fixed w-full z-50"
-  >
-    <div className="container mx-auto flex justify-between items-center">
-      <h1 className="text-2xl font-bold">
-        <img src={Mavlinlogo} alt="Mavlin Logo" className="w-[70px]" />
-      </h1>
-      <div className="flex space-x-8">
-        <Link to="/" className="hover:text-gray-400">
-          Home
-        </Link>
-        <Link to="/about" className="hover:text-gray-400">
-          About
-        </Link>
-        <Link to="/projects" className="hover:text-gray-400">
-          Projects
-        </Link>
+const menuItems = [
+  { title: "Home", path: "/" },
+  { title: "About", path: "/about" },
+  { title: "Projects", path: "/projects" },
+];
+
+const Header = () => {
+  return (
+    <motion.nav
+      initial={{ y: -100 }}
+      animate={{ y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="bg-white text-mavlin-blue px-24 py-4 fixed w-full z-50"
+    >
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">
+          <img src={Mavlinlogo} alt="Mavlin Logo" className="w-[70px]" />
+        </h1>
+        <nav className="flex space-x-8">
+          {menuItems.map((item) => (
+            <Link
+              key={item.path}
+              to={item.path}
+              className={({ isActive }) =>
+                `hover:text-gray-400 ${
+                  isActive ? "font-bold border-b-2 border-mavlin-blue" : ""
+                }`
+              }
+            >
+              {item.title}
+            </Link>
+          ))}
+        </nav>
+        <button className="bg-mavlin-blue text-white px-4 py-2">
+          Contact Us
+        </button>
       </div>
-      <button className="bg-mavlin-blue text-white px-4 py-2">
-        Contact Us
-      </button>
-    </div>
-  </motion.nav>
-);
+    </motion.nav>
+  );
+};
 
 export default Header;
