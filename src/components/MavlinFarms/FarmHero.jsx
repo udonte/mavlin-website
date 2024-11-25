f0f38976e1c3135e5e3e2f7a754db1c01ee19e16
George Udonte <georgeudonte@gmail.com>
2024-11-25 12:45:48 +0100

    Add missing Hero1.jpg image

--- src/components/MavlinFarms/FarmHero.jsx
+++ src/components/MavlinFarms/FarmHero.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
-import HeroImage1 from "../../assets/images/MavlinFarms/Hero1.jpg";
-import HeroImage2 from "../../assets/images/MavlinFarms/Hero2.jpg";
-import HeroImage3 from "../../assets/images/MavlinFarms/Hero3.jpg";
+import HeroImage1 from "../../assets/images/MavlinFarms/hero1.jpg";
+import HeroImage2 from "../../assets/images/MavlinFarms/hero2.jpg";
+import HeroImage3 from "../../assets/images/MavlinFarms/hero3.jpg";
 
 // Mock data for images and their associated text
 const slides = [
